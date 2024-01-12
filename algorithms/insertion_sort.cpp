f00cf71e421baf76c761dfdec253a72e437f07ee
Ozan <118054950+ozan2003@users.noreply.github.com>
2024-01-12 19:22:44 +0300

    new algorithm

---
+++ algorithms/insertion_sort.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <random>
+
+const auto seed = std::random_device{}();
+
+const auto rnd = [gen = std::mt19937{seed}, dist = std::uniform_int_distribution{1, 25}]() mutable -> int
+{
+    return dist(gen);
+};
+
+/**
+ * Sorts the elements in-place using the insertion sort algorithm.
+ *
+ * @param vec The vector to be sorted.
+ */
+void insertion_sort(std::vector<int>& vec)
+{
+    // Go left to right.
+    for (auto curr = vec.begin(); curr != vec.end(); ++curr)
+    {
+        // Check each element and compare it to the elements to its left.
+        // Track our previous element in case we need to swap.
+        for (auto prev = curr - 1; prev >= vec.begin(); --prev)
+        {
+            // Swap if the previous element is greater than the current element
+            // until we're at the beginning.
+            if (*prev > *(prev + 1))
+            {
+                std::iter_swap(prev, prev + 1);
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> vec(10);
+    std::generate(vec.begin(), vec.end(), rnd);
+
+    std::cout << "Before: ";
+    for (const auto& item : vec)
+    {
+        std::cout << item << ' ';
+    }
+
+    insertion_sort(vec);
+
+    std::cout << "\nAfter: ";
+    for (const auto& item : vec)
+    {
+        std::cout << item << ' ';
+    }
+    std::cout << '\n';
+}
