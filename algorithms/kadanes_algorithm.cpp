fc148eb3cc356ab6c80d7baa44a16f25734c207e
Ozan <118054950+ozan2003@users.noreply.github.com>
2024-09-15 20:36:32 +0300

    new stuff

---
+++ algorithms/kadanes_algorithm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <vector>
+
+// Kadane's algorithm.
+template <typename T>
+    requires std::is_arithmetic_v<T> || std::is_floating_point_v<T>
+T max_subarray_sum(const std::vector<T>& arr)
+{
+    auto best_sum = std::numeric_limits<T>::min();
+
+    for (T current_sum{}; const auto& value : arr)
+    {
+        // Accumulate the numbers.
+        current_sum = std::max(value, current_sum + value);
+        best_sum    = std::max(best_sum, current_sum); // Take the peak sum.
+    }
+    return best_sum;
+}
+
+int main()
+{
+    const std::vector arr{-1, 2, 4, -3, 5, 2, -5, 2};
+    const auto        result = max_subarray_sum(arr);
+
+    std::cout << "Max subarray sum: " << result << '\n';
+}
---
+++ algorithms/sliding_window.cpp
@@ -0,0 +1,42 @@
+#include <deque>
+#include <generator>
+#include <iostream>
+#include <ranges>
+
+template <std::ranges::range R>
+auto sliding_window(R&& rng, const std::size_t n)
+    -> std::generator<std::deque<std::ranges::range_reference_t<R>>>
+{
+    if (n == 0 || std::ranges::empty(rng) || n > std::ranges::size(rng))
+    {
+        co_return; // Empty generator (no windows to yield).
+    }
+
+    // Window with the first n elements.
+    std::deque<std::ranges::range_reference_t<R>> window(rng.cbegin(),
+                                               std::next(rng.cbegin(), n));
+    // Iterate over until the end hits the range's end.
+    for (auto it = std::next(rng.cbegin(), n); it != rng.cend();
+         std::advance(it, 1))
+    {
+        co_yield window;
+        // Move the window one step forward.
+        window.pop_front();    // Pop the front element.
+        window.push_back(*it); // Append the next element.
+    }
+    co_yield window; // Yield the last window.
+}
+
+int main()
+{
+    auto rng = std::views::iota(1, 10 + 1);
+    // Just printing.
+    for (const auto& window : sliding_window(rng, 5))
+    {
+        for (const auto& i : window)
+        {
+            std::cout << i << ' ';
+        }
+        std::cout << '\n';
+    }
+}
