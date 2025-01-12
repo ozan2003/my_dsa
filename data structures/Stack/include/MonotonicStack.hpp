b8ab1e0bcdea489477f9d149d4dac1d2bb68bd73
Ozan Malci <ozan41094@gmail.com>
2025-01-12 10:52:46 +0300

    comments

--- data structures/Stack/include/MonotonicStack.hpp
+++ data structures/Stack/include/MonotonicStack.hpp
@@ -7,10 +7,12 @@
 #include <type_traits>
 
 // A monotonic stack is one in which the elements maintain their monotonicity.
-// Their items comply with Comp. For instance, if Comp is std::less, stack items will decrease.
-// If Comp is std::greater, the stack items increase.
-// You can think of it as an ordered stack.
-template <typename T, typename Container = std::deque<T>, typename Comp = std::less<T>>
+// Their items comply with Comp. For instance, if Comp is std::less, stack items
+// will decrease. If Comp is std::greater, the stack items increase. You can
+// think of it as an ordered stack.
+template <typename T,
+          typename Container = std::deque<T>,
+          typename Comp      = std::less<T>>
     requires std::predicate<Comp, const T&, const T&>
 class MonotonicStack : public std::stack<T, Container>
 {
@@ -25,6 +27,10 @@ private:
     Comp comp{};
 
 public:
+    /**
+     * @brief Adds an item to the stack.
+     * @param val The item to add to the stack.
+     */
     constexpr void push(const_reference val)
     {
         while (!this->empty() && !comp(val, this->top()))
@@ -35,6 +41,10 @@ public:
         std::stack<value_type>::push(val);
     }
 
+    /**
+     * @brief Adds an item to the stack.
+     * @param val The item to add to the stack.
+     */
     constexpr void push(value_type&& val)
     {
         while (!this->empty() && !comp(val, this->top()))
@@ -45,9 +55,21 @@ public:
         std::stack<value_type>::push(std::move(val));
     }
 
+    /**
+     * @brief Adds an item to the stack.
+     * @param args The arguments to use to construct the item.
+     * @return The item added to the stack.
+     */
     template <typename... Args>
     constexpr decltype(auto) emplace(Args&&... args)
     {
+        /*
+         * In order to maintain the monotonicity of the stack, I create the
+         * item twice.
+         *
+         * Once to compare with the top of the stack, and once to
+         * emplace onto the stack.
+         */
         const value_type val(std::forward<Args>(args)...);
         while (!this->empty() && !comp(val, this->top()))
         {
