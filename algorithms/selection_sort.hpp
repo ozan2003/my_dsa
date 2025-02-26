5450fc7ba0dc92afc9c1a3bd8839d1633379fd04
Ozan Malci <ozan41094@gmail.com>
2025-02-26 20:35:58 +0300

    use range concept and explain the algorithm

--- algorithms/selection_sort.hpp
+++ algorithms/selection_sort.hpp
@@ -2,28 +2,37 @@
 
 #include <algorithm> // std::min_element, std::iter_swap
 #include <concepts>  // std::totally_ordered, std::predicate
-#include <utility>   // std::forward
-#include <vector>    // std::vector
+#include <ranges>  // std::ranges::contiguous_range, std::ranges::range_value_t
+#include <utility> // std::forward
 
 /**
  * Sorts the elements in-place using the selection sort algorithm.
  * Note that since it has O(n^2) time complexity, it's not suitable for large
  * containers.
  *
- * @param vec The vector to be sorted.
+ * @param seq The sequence to be sorted.
  */
-template <std::totally_ordered T, std::predicate<T, T> Pred = std::less<T>>
-void selection_sort(std::vector<T>& vec, Pred&& pred = Pred{})
+template <std::ranges::contiguous_range R,
+          typename T                = std::ranges::range_value_t<R>,
+          std::predicate<T, T> Pred = std::less<T>>
+void selection_sort(R& seq, Pred&& pred = Pred{})
 {
+    // 1. Start from the beginning of the sequence.
+    // 2. Find the smallest element in the range [front, seq.end()).
+    // 3. Swap the smallest element with the first element in the range.
+    // 4. Move the front pointer to the next element.
+    // 5. Repeat the process until the front pointer reaches the end of the
+    //    sequence.
     // There's no need to look at the last element since the last comparison
     // always with itself.
-    for (auto it = vec.begin(); it < vec.end() - 1; ++it)
+    for (auto front = seq.begin(); front < seq.end() - 1; ++front)
     {
-        // Find the smallest element in the range [it++, vec.end()).
-        auto min_item =
-            std::min_element(it, vec.end(), std::forward<Pred>(pred));
-        // Swap the smallest element with the first element in the range [it++,
-        // vec.end()) through iterators.
-        std::iter_swap(min_item, it);
+        // Find the smallest element in the range [front++, seq.end()).
+        auto min_item = std::ranges::min_element(front,
+                                                 seq.end(),
+                                                 std::forward<Pred>(pred));
+        // Swap the smallest element with the first element in the range
+        // [front++, seq.end()) through iterators.
+        std::iter_swap(min_item, front);
     }
 }
