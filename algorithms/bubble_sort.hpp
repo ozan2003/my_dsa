a54d28014f37f791eb046b93e2f996bc39945ac2
Ozan Malci <ozan41094@gmail.com>
2025-03-12 22:26:06 +0300

    Refactor bubble_sort to use std::ranges and improve template parameters

--- algorithms/bubble_sort.hpp
+++ algorithms/bubble_sort.hpp
@@ -3,33 +3,36 @@
 #include <algorithm>
 #include <concepts>
 #include <utility>
-#include <vector>
+// #include <vector>
+#include <ranges>
 
 /**
  * The bubble sort algorithm.
- * The container will be sorted in-place.
+ * The range will be sorted in-place.
  *
- * @param vec The vector to be sorted.
+ * @param rng The range to be sorted.
  * @param func The comparison function.
  *             The signature should be equivalent to the following:
  *             bool cmp(const T& a, const T& b);
  */
-template <typename T, typename Pred = std::less<T>>
+template <std::ranges::contiguous_range R,
+          typename T    = std::ranges::range_value_t<R>,
+          typename Pred = std::less<T>>
     requires std::totally_ordered<T> && std::predicate<Pred, const T&, const T&>
-void bubble_sort(std::vector<T>& vec, Pred&& pred = Pred{})
+void bubble_sort(R& rng, Pred&& pred = Pred{})
 {
     // The last element will already be in the correct position.
-    auto end = vec.end() - 1;
+    auto end = rng.end() - 1;
 
     // Make the predicate usable for move-only types.
     auto&& predicate = std::forward<Pred>(pred);
 
     // The comparator fails when end is at the beginning of the vector
     // since there is no element in range [begin, end).
-    while (vec.begin() != end)
+    while (rng.begin() != end)
     {
         // Starting from the beginning, compare every adjacent pair.
-        for (auto beg = vec.begin(); beg != end; ++beg)
+        for (auto beg = rng.begin(); beg != end; ++beg)
         {
             // Swap them if they are in the wrong order.
             if (auto next = std::next(beg); !predicate(*beg, *next))
