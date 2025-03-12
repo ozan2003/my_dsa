8ec969a35802ebe89f38484d95d1d1ce22d5e717
Ozan Malci <ozan41094@gmail.com>
2025-03-12 22:25:52 +0300

    Refactor merge_sort to use std::ranges and improve template parameters

--- algorithms/merge_sort.hpp
+++ algorithms/merge_sort.hpp
@@ -5,8 +5,6 @@
 #include <iterator>
 #include <ranges>
 
-namespace rs = std::ranges;
-
 /**
  * The merge sort algorithm.
  * The range will be sorted in-place.
@@ -18,12 +16,10 @@ namespace rs = std::ranges;
  *             your own predicate, its signature should be equivalent to
  *            `bool pred(const T&, const T&)`.
  */
-template <std::bidirectional_iterator Iter, typename Pred = std::less<>>
-    requires std::predicate<Pred,
-                            std::iter_value_t<Iter>,
-                            std::iter_value_t<Iter>>
-             && std::totally_ordered<std::iter_value_t<Iter>>
-             //&& std::sortable<Iter, Pred>
+template <std::bidirectional_iterator Iter,
+          typename T    = std::iter_value_t<Iter>,
+          typename Pred = std::less<T>>
+    requires std::predicate<Pred, T, T> && std::totally_ordered<T>
 void merge_sort(const Iter& begin, const Iter& end, Pred&& pred = Pred{})
 {
     if (std::distance(begin, end) <= 1)
@@ -33,14 +29,13 @@ void merge_sort(const Iter& begin, const Iter& end, Pred&& pred = Pred{})
 
     // Split the range into two halves.
     const auto         mid = std::next(begin, std::distance(begin, end) / 2);
-    const rs::subrange left(begin, mid);
-    const rs::subrange right(mid, end);
+    const std::ranges::subrange left(begin, mid);
+    const std::ranges::subrange right(mid, end);
 
     // Recursively sort the two halves.
     merge_sort(left.begin(), left.end(), pred);
     merge_sort(right.begin(), right.end(), pred);
 
     // Merge the halves sorted.
-    rs::inplace_merge(left.begin(), mid, right.end(), pred);
+    std::ranges::inplace_merge(left.begin(), mid, right.end(), pred);
 }
-
