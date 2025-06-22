a3b73e550caf73316c18b59faff21fee3580a066
Ozan Malci <ozan41094@gmail.com>
2025-06-22 19:50:19 +0300

    clarity

--- algorithms/floyd_warshall.rs
+++ algorithms/floyd_warshall.rs
@@ -1,18 +1,18 @@
 mod algorithm
 {
     use std::cmp::Ordering;
-    use std::fmt::Display;
+    use std::fmt::{Display, Result as FmtResult};
     use std::ops::Add;
 
     #[derive(PartialEq, Eq, Clone, Copy)]
     /// Represents a distance in a directed graph.
-    pub enum Distance
+    pub enum Dist
     {
-        Finite(u32),
+        Fin(u32),
         Inf,
     }
 
-    impl PartialOrd for Distance
+    impl PartialOrd for Dist
     {
         fn partial_cmp(&self, other: &Self) -> Option<Ordering>
         {
@@ -20,49 +20,48 @@ mod algorithm
         }
     }
 
-    impl Ord for Distance
+    impl Ord for Dist
     {
         fn cmp(&self, other: &Self) -> Ordering
         {
             match (self, other)
             {
-                (&Distance::Finite(a), &Distance::Finite(b)) => a.cmp(&b),
-                (&Distance::Inf, &Distance::Inf) => Ordering::Equal,
-                (&Distance::Finite(_), &Distance::Inf) => Ordering::Less,
-                (&Distance::Inf, &Distance::Finite(_)) => Ordering::Greater,
+                (&Dist::Fin(a), &Dist::Fin(b)) => a.cmp(&b),
+                (&Dist::Inf, &Dist::Inf) => Ordering::Equal,
+                (&Dist::Fin(_), &Dist::Inf) => Ordering::Less,
+                (&Dist::Inf, &Dist::Fin(_)) => Ordering::Greater,
             }
         }
     }
 
-    impl Add for Distance
+    impl Add for Dist
     {
-        type Output = Distance;
+        type Output = Dist;
 
         fn add(self, other: Self) -> Self::Output
         {
             match (self, other)
             {
-                (Distance::Finite(a), Distance::Finite(b)) => Self::Finite(a + b),
-                (Distance::Finite(_) | Distance::Inf, Distance::Inf) |
-                (Distance::Inf, Distance::Finite(_)) => Self::Inf,
+                (Dist::Fin(a), Dist::Fin(b)) => Self::Fin(a + b),
+                (Dist::Fin(_) | Dist::Inf, Dist::Inf) | (Dist::Inf, Dist::Fin(_)) => Self::Inf,
             }
         }
     }
 
-    impl Display for Distance
+    impl Display for Dist
     {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult
         {
             match *self
             {
-                Distance::Finite(value) => write!(f, "{}", value),
-                Distance::Inf => write!(f, "Inf"),
+                Dist::Fin(value) => write!(f, "{:>3}", value),
+                Dist::Inf => write!(f, "{:>3}", "∞"),
             }
         }
     }
 
     /// A graph represented as an adjacency matrix.
-    pub type Graph<const N: usize> = [[Distance; N]; N];
+    pub type Graph<const N: usize> = [[Dist; N]; N];
 
     /// Computes the shortest paths between all pairs of vertices in a directed
     /// graph using the Floyd-Warshall algorithm[1].
